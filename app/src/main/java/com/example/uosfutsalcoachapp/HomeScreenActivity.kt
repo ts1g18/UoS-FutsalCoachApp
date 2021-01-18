2fdc45a96a589e30c1347940bd158d4340bb99c8
ts1g18 <ts1g18@soton.ac.uk>
2021-01-18 16:51:55 +0000

    - Implemented reading from users collection in cloud firestore - HomeScreenActivity.kt displays fullname in welcome message

--- app/src/main/java/com/example/uosfutsalcoachapp/HomeScreenActivity.kt
+++ app/src/main/java/com/example/uosfutsalcoachapp/HomeScreenActivity.kt
@@ -3,16 +3,49 @@ package com.example.uosfutsalcoachapp
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.firestore.DocumentReference
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 
 class HomeScreenActivity : AppCompatActivity() {
+    private lateinit var auth: FirebaseAuth
+    private lateinit var fStore : FirebaseFirestore
+    private val TAG = "HomeScreenActivity"
+    var userID = ""
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home_screen)
+        val welcomeMsg : TextView = findViewById(R.id.tv_welcome)
 
         System.err.println(callingActivity?.className)
+        // Initialize Firebase Auth and firestore
+        auth = Firebase.auth
+        fStore = Firebase.firestore
+        val user = Firebase.auth.currentUser
+        if (user != null) {
+            userID = user.uid
+        }
+       // val documentReference : DocumentReference = fStore.collection("users").document(userID)
+        fStore.collection("users").get().addOnSuccessListener { result ->
+                    for (document in result) {
+                        Log.d(TAG, "${userID} => ${document.data}")
+                        if(userID == document.id){
+                            welcomeMsg.setText("Welcome ${document.data.get("Full Name").toString()}")
+                        }
+                    }
+                }
+                .addOnFailureListener { exception ->
+                    Log.w(TAG, "Error getting documents.", exception)
+                }
         val btnLogout : Button = findViewById(R.id.btnLogout)
         btnLogout.setOnClickListener{
             //log the current user out and redirect him to the login activity
