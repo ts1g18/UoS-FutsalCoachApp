5f65a43ecae7cdb9b34d378df11501a31e1a1ff2
ts1g18 <ts1g18@soton.ac.uk>
2021-02-11 14:40:45 +0000

    - Added View Tactic and Delete Tactic buttons on bottom of View Tactic screen - Added functionality to delete tactic button -> Deletes from firebase firestore as well

--- app/src/main/java/com/example/uosfutsalcoachapp/MyAdapter.kt
+++ app/src/main/java/com/example/uosfutsalcoachapp/MyAdapter.kt
@@ -1,18 +1,28 @@
 package com.example.uosfutsalcoachapp
 
+import android.graphics.Color
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.core.view.isInvisible
 import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.activity_view_tactic.view.*
 import kotlinx.android.synthetic.main.tactic_item.view.*
 
-class MyAdapter(private val tacticList: List<TacticItem>) : RecyclerView.Adapter<MyAdapter.MyViewHolder>(){
+class MyAdapter(private val tacticList: MutableList<TacticItem>) :
+    RecyclerView.Adapter<MyAdapter.MyViewHolder>() {
+    private var selected_position = -1;
+    var removedPosition : Int? = null
+
     //called by recycler view when its time to create a new view holder
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
-        val itemView = LayoutInflater.from(parent.context).inflate(R.layout.tactic_item,
-        parent, false)
+        val itemView = LayoutInflater.from(parent.context).inflate(
+            R.layout.tactic_item,
+            parent, false
+        )
 
         return MyViewHolder(itemView)
     }
@@ -22,15 +32,32 @@ class MyAdapter(private val tacticList: List<TacticItem>) : RecyclerView.Adapter
         val currentItem = tacticList[position]
         holder.imageView.setImageResource(currentItem.imageResource) //we added these properties in constructor of TacticItem
         holder.textView1.text = currentItem.text1
+        if (selected_position == position) {
+            holder.itemView.setBackgroundColor(Color.parseColor("#DCDCDC"))
+        } else {
+            holder.itemView.setBackgroundColor(Color.parseColor("#ffffff"))
+        }
+        holder.itemView.setOnClickListener {
+            selected_position = position
+            notifyDataSetChanged()
+        }
+
     }
 
-    override fun getItemCount()= tacticList.size
+    override fun getItemCount() = tacticList.size
+
     //represents one row in our list
     //each view holder will hold references to the views in our row layout (tactic_item.xml)
-    class MyViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+    inner class MyViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         //create 3 properties that store the references to the 3 views in our row layout (tactic_item.xml)
         // this is how the viewHolder caches this references
         val imageView: ImageView = itemView.image_view //equivalent to findViewById
         val textView1: TextView = itemView.text_view_1
     }
+
+    fun getSelectedPosition() : Int? {
+        var position = selected_position
+        return position;
+    }
 }
+
--- app/src/main/java/com/example/uosfutsalcoachapp/ViewTacticActivity.kt
+++ app/src/main/java/com/example/uosfutsalcoachapp/ViewTacticActivity.kt
@@ -3,19 +3,30 @@ package com.example.uosfutsalcoachapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.firebase.ui.database.FirebaseRecyclerAdapter
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 import kotlinx.android.synthetic.main.activity_view_tactic.*
+import java.lang.Exception
 
 class ViewTacticActivity : AppCompatActivity() {
     private lateinit var auth: FirebaseAuth
     private lateinit var fStore: FirebaseFirestore
     private val TAG = "ViewTacticActivity"
-
+    private val tacticList = ArrayList<TacticItem>()
+    private val adapter = MyAdapter(tacticList)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,29 +37,65 @@ class ViewTacticActivity : AppCompatActivity() {
         auth = Firebase.auth
         fStore = Firebase.firestore
 
-       generateTacticList()
+        generateTacticList()
 
-
-
+        val deleteTacticBtn : Button = findViewById(R.id.btnDeleteTactic)
+        deleteTacticBtn.setOnClickListener{
+            deleteTactic()
+        }
     }
+
 
     /*
     * this method gets all of the tactics from the firestore and displays them in the recycler view list
      */
     private fun generateTacticList() {
-        val tacticList = ArrayList<TacticItem>()
         fStore.collection("tactics").get().addOnSuccessListener { result ->
             for (document in result) {
                 val drawable = R.drawable.ic_tactic_item
                 val item = TacticItem(drawable, "${document.id}")
                 tacticList += item
             }
-            recycler_view.adapter = MyAdapter(tacticList)
-            recycler_view.layoutManager = LinearLayoutManager(this) //LinearLayoutManager creates vertical scrolling list
+            recycler_view.adapter = adapter
+            recycler_view.layoutManager =
+                LinearLayoutManager(this) //LinearLayoutManager creates vertical scrolling list
             recycler_view.setHasFixedSize(true) //optimization
         }
+            .addOnFailureListener { exception ->
+                Log.w(TAG, "Error getting documents.", exception)
+            }
+    }
+
+    /*
+    * this method deletes the currently selected tactic
+     */
+    private fun deleteTactic() {
+        try {
+            //get the name of the tactic to be removed so that we can compare it wwith document.id from firestore and remove from dataabase as well
+            var tacticToBeRemoved = tacticList.get(adapter.getSelectedPosition()!!).text1
+            //remove it from list
+            tacticList.removeAt(adapter.getSelectedPosition()!!)
+            adapter.notifyItemRemoved(adapter.getSelectedPosition()!!)
+            fStore.collection("tactics").get().addOnSuccessListener { result ->
+                for (document in result) {
+                    if (document.id == tacticToBeRemoved) {
+                        fStore.collection("tactics").document(tacticToBeRemoved).delete()
+                            .addOnSuccessListener {
+                                Log.d(
+                                    TAG,
+                                    "DocumentSnapshot successfully deleted!"
+                                )
+                            }
+                            .addOnFailureListener { e -> Log.w(TAG, "Error deleting document", e) }
+                    }
+                }
+            }
                 .addOnFailureListener { exception ->
                     Log.w(TAG, "Error getting documents.", exception)
                 }
+        }catch(e : Exception){
+            Toast.makeText(this,"No tactic is currently selected",Toast.LENGTH_SHORT).show()
+        }
     }
+
 }
